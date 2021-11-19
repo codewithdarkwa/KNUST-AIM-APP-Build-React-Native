c0c5cd2c61e3e3a08cd8979d8e4f7ea879db3d7c
Darkwa <jnrdrkw1@gmail.com>
2021-11-19 21:07:28 +0000

    Removing unused content

--- App.js
+++ App.js
@@ -2,17 +2,9 @@ import React from 'react';
 import { NavigationContainer,DarkTheme, DefaultTheme } from '@react-navigation/native';
 import DrawerNavigator from './navigation/DrawerNavigator';
 import {ThemeProvider} from './hooks/ThemeContext'
-import {useTheme} from './hooks/ThemeContext'
 
 export default function App() {
-  const themeStyles ={
-    ...DarkTheme,
-    colors:{
-    backgroundColor: DarkTheme ? '#404040' : '#fff',
-        txtColor: '#fff',
-        icon: '#fff',
-    }
-    }
+  
   return (
       <ThemeProvider>
          <NavigationContainer theme={DarkTheme}>
